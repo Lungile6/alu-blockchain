6142bc3aeb27a7db50c40996543b61c81d96ef65
Lungile6 <l.mabelebel@alustudent.com>
2026-01-13 13:25:43 +0000

    Working on first task

---
+++ blockchain/v0.1/blockchain_create.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <string.h>
+#include "blockchain.h"
+
+/**
+ * blockchain_create - Creates a Blockchain structure and initializes it
+ * with the Genesis Block.
+ *
+ * Return: A pointer to the newly created blockchain_t, or NULL on failure.
+ */
+blockchain_t *blockchain_create(void)
+{
+    blockchain_t *blockchain;
+    block_t *genesis;
+    llist_t *list;
+
+    /* 1. Allocate the blockchain container */
+    blockchain = malloc(sizeof(blockchain_t));
+    if (!blockchain)
+        return (NULL);
+
+    /* 2. Create the linked list to hold our blocks */
+    list = llist_create(MT_DIRECT);
+    if (!list)
+    {
+        free(blockchain);
+        return (NULL);
+    }
+    blockchain->chain = list;
+
+    /* 3. Allocate the Genesis Block */
+    genesis = malloc(sizeof(block_t));
+    if (!genesis)
+    {
+        llist_destroy(blockchain->chain, 1, NULL);
+        free(blockchain);
+        return (NULL);
+    }
+
+    /* 4. Initialize Genesis Block Info */
+    genesis->info.index = 0;
+    genesis->info.difficulty = 0;
+    genesis->info.timestamp = 1537578000;
+    genesis->info.nonce = 0;
+    memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
+
+    /* 5. Initialize Genesis Block Data */
+    memset(genesis->data.buffer, 0, BLOCKCHAIN_DATA_MAX);
+    memcpy(genesis->data.buffer, "Holberton School", 16);
+    genesis->data.len = 16;
+
+    /* 6. Set Genesis Block Hash */
+    memcpy(genesis->hash,
+           "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
+           "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03",
+           SHA256_DIGEST_LENGTH);
+
+    /* 7. Add the Genesis Block to the chain */
+    if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) != 0)
+    {
+        free(genesis);
+        llist_destroy(blockchain->chain, 1, NULL);
+        free(blockchain);
+        return (NULL);
+    }
+
+    return (blockchain);
+}
