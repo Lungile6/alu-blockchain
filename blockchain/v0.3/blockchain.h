5db70d3b0d768e3741db556edefa6b22f42c45d7
Lungile6 <l.mabelebel@alustudent.com>
2026-01-23 08:41:09 +0000

    Task one debugged

--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -5,20 +5,16 @@
 #include <time.h>
 #include <llist.h>
 
-/* * 1. Include the transaction header. 
- * Use the relative path to ensure the compiler finds it.
- */
+/* 1. Include transaction.h FIRST so transaction_t is known */
 #include "transaction/transaction.h"
 #include "../../crypto/hblk_crypto.h"
 
 /* Constants */
-#define GENESIS_TIMESTAMP 1537578000
-#define GENESIS_DATA "Holberton School"
-#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+#define SHA256_DIGEST_LENGTH 32
+#define BLOCKCHAIN_DATA_MAX 1024
 
-/**
- * struct block_info_s - Block info structure
- */
+/* --- Struct Definitions --- */
+
 typedef struct block_info_s
 {
 	uint32_t    index;
@@ -28,35 +24,33 @@ typedef struct block_info_s
 	uint8_t     prev_hash[SHA256_DIGEST_LENGTH];
 } block_info_t;
 
-/**
- * struct block_s - Block structure
- */
+/* The printer expects this struct to exist */
+typedef struct block_data_s
+{
+	int8_t      buffer[BLOCKCHAIN_DATA_MAX];
+	uint32_t    len;
+} block_data_t;
+
 typedef struct block_s
 {
 	block_info_t    info;
-	llist_t         *transactions;
+	block_data_t    data;         /* Keep this for the printer's sake */
+	llist_t         *transactions; /* The v0.3 addition */
 	uint8_t         hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
-/**
- * struct blockchain_s - Blockchain structure
- */
 typedef struct blockchain_s
 {
 	llist_t     *chain;
 	llist_t     *unspent;
 } blockchain_t;
 
-/* --- v0.1 & v0.2 Functions --- */
-blockchain_t *blockchain_create(void);
-void blockchain_destroy(blockchain_t *blockchain);
-void block_destroy(block_t *block);
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
-int block_is_valid(block_t const *block, block_t const *prev_block);
+/* --- Prototypes for the Printer --- */
+/* Note: These must match the signatures in _blockchain_print.c exactly */
+void _blockchain_print(blockchain_t const *blockchain);
+void _blockchain_print_brief(blockchain_t const *blockchain);
 
-/* --- v0.3 Functions --- */
-/* * NOTE: Do NOT include _block_print or _transaction_print_loop here.
- * Those are defined as STATIC in the .c file and cannot be declared here.
- */
+/* Note: These are 'static' in the .c file, so we don't declare them here 
+   unless they are called by other files. */
 
 #endif /* BLOCKCHAIN_H */
