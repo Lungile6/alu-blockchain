aac2254b5d59597f64b35f289549f4e8653dfa96
Lungile6 <l.mabelebel@alustudent.com>
2026-01-24 19:30:51 +0000

    11th task done

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -1,33 +1,49 @@
 #include <stdlib.h>
+#include <string.h>
 #include "blockchain.h"
-#include "hblk_crypto.h"
 
 /**
- * block_hash - Computes the hash of a Block
- *
+ * block_hash - Computes the hash of a Block including its transactions
  * @block: Pointer to the Block to be hashed
- * @hash_buf: Buffer where the resulting hash must be stored
+ * @hash_buf: Buffer to store the resulting hash
  *
- * Return: A pointer to hash_buf
+ * Return: Pointer to hash_buf
  */
 uint8_t *block_hash(block_t const *block,
-		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len;
+	size_t size, offset;
+	int i, tx_count;
+	uint8_t *buf;
+	transaction_t *tx;
 
-	if (!block || !hash_buf)
+	if (!block)
+		return (NULL);
+	tx_count = llist_size(block->transactions);
+	if (tx_count < 0)
+		tx_count = 0;
+
+	size = sizeof(block->info) + block->data.len +
+		(tx_count * SHA256_DIGEST_LENGTH);
+	buf = malloc(size);
+	if (!buf)
 		return (NULL);
 
-	/**
-	     * Calculate length: block_info_t size + the actual data length stored.
-	 * Since 'info' and 'data' are the first two fields in block_t,
-	 * we hash from the start of the block.
-	 */
-	len = sizeof(block->info) + block->data.len;
+	/* Copy Info and Data */
+	memcpy(buf, &block->info, sizeof(block->info));
+	offset = sizeof(block->info);
+	memcpy(buf + offset, block->data.buffer, block->data.len);
+	offset += block->data.len;
 
-	/* sha256 function is provided in the hblk_crypto library */
-	if (!sha256((int8_t const *)block, len, hash_buf))
-		return (NULL);
+	/* Copy each Transaction ID */
+	for (i = 0; i < tx_count; i++)
+	{
+		tx = llist_get_node_at(block->transactions, i);
+		memcpy(buf + offset, tx->id, SHA256_DIGEST_LENGTH);
+		offset += SHA256_DIGEST_LENGTH;
+	}
 
+	sha256((int8_t const *)buf, size, hash_buf);
+	free(buf);
 	return (hash_buf);
 }
--- blockchain/v0.3/blockchain.h
+++ blockchain/v0.3/blockchain.h
@@ -77,6 +77,8 @@ int block_is_valid(block_t const *block, block_t const *prev_block);
 /* int _block_print(block_t const *block, unsigned int index, char const *prefix); */
 block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len);
 void block_destroy(block_t *block);
+/* block_hash.c */
+uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 
 /* --- Printer Prototypes --- */
 /* transaction_t is recognized here because of the guard at the top */
